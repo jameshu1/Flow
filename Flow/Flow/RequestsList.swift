742d87ba494faf726e4547bc30daf1d7f9171e34
jameshu <jameshu1@uw.edu>
2020-10-17 16:29:57 -0700

    his

---
+++ Flow/Flow/RequestsList.swift
@@ -0,0 +1,20 @@
+//
+//  RequestsList.swift
+//  Flow
+//
+//  Created by James Hu on 10/17/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct RequestsList: View {
+    var body: some View {
+        List {
+            HStack {
+                
+                
+            }
+        }
+    }
+}
